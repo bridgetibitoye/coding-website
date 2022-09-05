b237d819732f341c4539cd5cfd65f03ad8c1212f
bridgetibitoye <101595088+bridgetibitoye@users.noreply.github.com>
2022-09-05 15:48:10 -0400

    Update profile.js

--- src/profile.js
+++ src/profile.js
@@ -9,7 +9,7 @@ const animation = {
 };
 // Change your display name on tha landing display
 const header = {
-  name: "a Human",
+  name: "Bridget Ibitoye",
 };
 const background = {
   // Options: Snow or Particle
@@ -20,7 +20,7 @@ const background = {
 const section2title = "About Me";
 const about = {
   paragraph:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris nunc congue nisi vitae suscipit tellus mauris a diam. ",
+    "I'm a full-time student at Father Bressani CHS based in Toronto. I have a profound interest in full-stack development, machine learning, human-computer interactions, and everything in between.. ",
 };
 // Edit your skill and the percentage you know about it
 // To Add a skill, copy any one below and paste it after the last comma
